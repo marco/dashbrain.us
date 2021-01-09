61a27c7be4dd029eac86ee52932443c87ee6e4f4
Marco Burstein <marco@marco.how>
2021-01-09 10:18:30 -0800

    Update `Event` component to use new, specific-event components.

--- frontend/components/events/Event.tsx
+++ frontend/components/events/Event.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Loading from '../Loading';
 import { Event, getSenderDetails } from '../../lib/events';
 import { Room } from '../../lib/rooms';
+import RaiseHandEventComponent from './RaiseHandEvent';
+import QuestionEvent from './QuestionEvent';
 
 let EventComponent: React.FC<{ event: Event; room: Room }> = (props) => {
   let senderDetails = getSenderDetails(props.event, props.room);
 
   if (props.event.type === 'hand') {
-    if (senderDetails.isCurrentUser) {
-      return <div>You raised your hand.</div>;
-    } else {
-      // TODO: Include photo if available.
-      return <div>{senderDetails.name} raised their hand.</div>;
-    }
+    return <RaiseHandEventComponent senderDetails={senderDetails} />;
+  }
+
+  if (props.event.type === 'question') {
+    return <QuestionEvent event={props.event} senderDetails={senderDetails} />;
   }
 
   // TODO: Other event types.
