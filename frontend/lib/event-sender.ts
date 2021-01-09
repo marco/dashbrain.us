45125e113a5526f7f23698f86979064019322201
Marco Burstein <marco@marco.how>
2021-01-09 10:18:39 -0800

    Create `askQuestion` lib method.

--- frontend/lib/event-sender.ts
+++ frontend/lib/event-sender.ts
@@ -25,6 +25,33 @@ export async function raiseHand(room: rooms.Room): Promise<events.Event> {
   return event;
 }
 
+export async function askQuestion(
+  room: rooms.Room,
+  text: string
+): Promise<events.Event> {
+  let reference = firebase
+    .firestore()
+    .collection('rooms')
+    .doc(room.id)
+    .collection('events')
+    .doc();
+
+  let event: events.Event = {
+    id: reference.id,
+    type: 'question',
+    text: text,
+    when: firebase.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp,
+    senderUid: firebase.auth().currentUser?.uid as string,
+    recipientUids: [
+      room.teacherUid,
+      firebase.auth().currentUser?.uid as string,
+    ],
+  };
+
+  await reference.set(event);
+  return event;
+}
+
 export async function deleteEvent(
   room: rooms.Room,
   event: events.Event
