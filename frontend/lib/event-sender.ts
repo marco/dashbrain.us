da9024daf991e03f145356ac8e9966667dadf37f
Marco Burstein <marco@marco.how>
2021-01-09 10:33:22 -0800

    Create `upvoteQuestion` function.

--- frontend/lib/event-sender.ts
+++ frontend/lib/event-sender.ts
@@ -52,6 +52,33 @@ export async function askQuestion(
   return event;
 }
 
+export async function upvoteQuestion(
+  room: rooms.Room,
+  questionEventId: string
+): Promise<events.Event> {
+  let reference = firebase
+    .firestore()
+    .collection('rooms')
+    .doc(room.id)
+    .collection('events')
+    .doc();
+
+  let event: events.Event = {
+    id: reference.id,
+    type: 'question_upvote',
+    questionEventId: questionEventId,
+    when: firebase.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp,
+    senderUid: firebase.auth().currentUser?.uid as string,
+    recipientUids: [
+      room.teacherUid,
+      firebase.auth().currentUser?.uid as string,
+    ],
+  };
+
+  await reference.set(event);
+  return event;
+}
+
 export async function deleteEvent(
   room: rooms.Room,
   event: events.Event
