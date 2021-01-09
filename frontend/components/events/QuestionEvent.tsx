7ec74ff98216308d5205aaed7ad1fecb8ecd42fc
Marco Burstein <marco@marco.how>
2021-01-09 10:44:40 -0800

    Update `QuestionEvent` component to include upvote count.

--- frontend/components/events/QuestionEvent.tsx
+++ frontend/components/events/QuestionEvent.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import { EventQuestion, SenderDetails } from '../../lib/events';
+import React, { useState } from 'react';
+import { Event, EventQuestion, SenderDetails } from '../../lib/events';
+import * as eventSender from '../../lib/event-sender';
+import { Room } from '../../lib/rooms';
+import pluralize from 'pluralize';
 
 let QuestionEvent: React.FC<{
+  room: Room;
   event: EventQuestion;
   senderDetails: SenderDetails;
+  events: Event[];
 }> = (props) => {
+  let [hasUpvoted, setHasUpvoted] = useState(false);
+  let upvoteCount = getUpvoteCount();
+
   return (
     <div>
-      {props.senderDetails.name} asked, &ldquo;{props.event.text}&rdquo;
+      {props.senderDetails.name} asked, &ldquo;{props.event.text}&rdquo;{' '}
+      {upvoteCount > 0 ? pluralize('Likes', upvoteCount, true) + ' ' : ''}
+      <button onClick={onClickUpvote} disabled={hasUpvoted}>
+        Upvote
+      </button>
     </div>
   );
+
+  function getUpvoteCount(): number {
+    return props.events.filter(
+      (event) =>
+        event.type === 'question_upvote' &&
+        event.questionEventId === props.event.id
+    ).length;
+  }
+
+  async function onClickUpvote() {
+    setHasUpvoted(true);
+    eventSender.upvoteQuestion(props.room, props.event.id);
+  }
 };
 
 export default QuestionEvent;
