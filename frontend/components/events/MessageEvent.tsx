341a36bbb941b1b87ee050f3897db1458235ec5b
Marco Burstein <marco@marco.how>
2021-01-09 16:41:13 -0800

    Create `MessageEvent` component.

---
+++ frontend/components/events/MessageEvent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { EventMessage, SenderDetails } from '../../lib/events';
+import { Room } from '../../lib/rooms';
+
+let MessageEvent: React.FC<{
+  room: Room;
+  event: EventMessage;
+  senderDetails: SenderDetails;
+}> = (props) => {
+  return (
+    <div>
+      <p>New message from {props.senderDetails.name}</p>
+      <p>{props.event.text}</p>
+      <p>Click to view &amp; reply</p>
+    </div>
+  );
+};
+
+export default MessageEvent;
