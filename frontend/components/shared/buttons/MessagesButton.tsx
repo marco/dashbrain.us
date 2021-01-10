9ba010f86e5e91dea9ed3e91d53f1b9be85a5568
Marco Burstein <marco@marco.how>
2021-01-09 17:57:31 -0800

    Update messages button to remove `MessagesSheet` connection.

--- frontend/components/shared/buttons/MessagesButton.tsx
+++ frontend/components/shared/buttons/MessagesButton.tsx
@@ -1,23 +1,10 @@
 import React, { useState } from 'react';
 import { Event } from '../../../lib/events';
 import { Room } from '../../../lib/rooms';
-import MessagesSheet from '../MessagesSheet';
+import MessagesSheet from '../sheets/MessagesSheet';
 
-let MessagesButton: React.FC<{ room: Room; events: Event[] }> = (props) => {
-  let [showSheet, setShowSheet] = useState(false);
-
-  return (
-    <>
-      <button onClick={() => setShowSheet(true)}>Messages</button>
-      {showSheet ? (
-        <MessagesSheet
-          room={props.room}
-          events={props.events}
-          onClose={() => setShowSheet(false)}
-        />
-      ) : null}
-    </>
-  );
+let MessagesButton: React.FC<{ onClick: () => void }> = (props) => {
+  return <button onClick={props.onClick}>Messages</button>;
 };
 
 export default MessagesButton;
