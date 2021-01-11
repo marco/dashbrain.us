2bf872dc86896b63c83b440e7d3807013b565ab0
Marco Burstein <marco@marco.how>
2021-01-10 19:39:26 -0800

    Create styled `RaiseHandEvent` component.

--- frontend/components/events/RaiseHandEvent/index.tsx
+++ frontend/components/events/RaiseHandEvent/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { SenderDetails } from '../../../lib/events';
+import styles from './event.module.scss';
+import eventStyles from '../events.module.scss';
+import classNames from 'classnames';
 
 let RaiseHandEvent: React.FC<{
   senderDetails: SenderDetails;
   className?: string;
 }> = (props) => {
+  let image = (
+    <img
+      src="/assets/hand/white.png"
+      alt="Raise Hand"
+      className={eventStyles.iconSmall}
+    />
+  );
+
   if (props.senderDetails.isCurrentUser) {
-    return <div className={props.className}>You raised your hand.</div>;
+    return (
+      <div className={classNames(props.className, styles.event)}>
+        {image}
+        <p className="font-bold">You</p>
+        <p className="-mt-1.5">raised your hand.</p>
+      </div>
+    );
   } else {
     // TODO: Include photo if available.
     return (
-      <div className={props.className}>
-        {props.senderDetails.name} raised their hand.
+      <div className={classNames(props.className, styles.event)}>
+        {image}
+        <p className="font-bold">{props.senderDetails.name}</p>
+        <p className="-mt-1.5">raised their hand.</p>
       </div>
     );
   }
