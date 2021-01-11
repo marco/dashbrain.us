ad905d7e366e446f7604357548e67926abf2effa
Marco Burstein <marco@marco.how>
2021-01-10 19:56:20 -0800

    Create lowering student hand functionality.

--- frontend/components/events/RaiseHandEvent/index.tsx
+++ frontend/components/events/RaiseHandEvent/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { SenderDetails } from '../../../lib/events';
+import { EventHand, SenderDetails } from '../../../lib/events';
 import styles from './event.module.scss';
 import eventStyles from '../events.module.scss';
 import classNames from 'classnames';
+import { Room } from '../../../lib/rooms';
+import firebase from 'firebase/app';
+import * as eventSender from '../../../lib/event-sender';
 
 let RaiseHandEvent: React.FC<{
   senderDetails: SenderDetails;
   className?: string;
+  room: Room;
+  event: EventHand;
 }> = (props) => {
   let image = (
     <img
@@ -27,12 +32,26 @@ let RaiseHandEvent: React.FC<{
   } else {
     // TODO: Include photo if available.
     return (
-      <div className={classNames(props.className, styles.event)}>
+      <div
+        className={classNames(props.className, styles.event, 'cursor-pointer')}
+        onClick={onClick}
+      >
         {image}
         <p className="font-bold">{props.senderDetails.name}</p>
         <p className="-mt-1.5">raised their hand.</p>
+        {firebase.auth().currentUser?.uid === props.room.teacherUid ? (
+          <p className="text-xs mt-2.5">Click to lower their hand.</p>
+        ) : undefined}
       </div>
     );
+  }
+
+  async function onClick() {
+    if (firebase.auth().currentUser?.uid !== props.room.teacherUid) {
+      return;
+    }
+
+    await eventSender.deleteEvent(props.room, props.event);
   }
 };
 
