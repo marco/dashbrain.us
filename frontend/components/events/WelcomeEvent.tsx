7292007f36c8c16f2eb2fe6a0a452f6bb30458c6
Marco Burstein <marco@marco.how>
2021-01-10 17:22:17 -0800

    Create `WelcomeEvent` component.

---
+++ frontend/components/events/WelcomeEvent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { EventStudentJoin } from '../../lib/events';
+import { Room } from '../../lib/rooms';
+
+let WelcomeEvent: React.FC = (props) => {
+  return <div>Welcome to Dashbrain!</div>;
+};
+
+export default WelcomeEvent;
