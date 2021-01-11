c0fb4b150a1df1f9389cd7a5b3942be22ee332d9
Marco Burstein <marco@marco.how>
2021-01-10 16:32:17 -0800

    Create new index page and styles.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,70 @@ import Head from 'next/head';
 import config from '../frontend/config/config.json';
 import firebase from 'firebase/app';
 import React from 'react';
+import styles from '../styles/pages/index.module.scss';
+import LogoType from '../frontend/components/LogoType';
+import classNames from 'classnames';
+import FeatureItem from '../frontend/components/index/FeatureItem';
+import IndexButton from '../frontend/components/index/Button';
 
 let Home: React.FC = () => {
   return (
-    <div>
+    <div className={classNames(styles.bodyDiv, 'text-white overflow-hidden')}>
       <Head>
         <title>{config.appName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="text-white text-center pt-12 pb-20 px-6 relative">
-        <div className="transform -skew-y-6 bg-blue-600 -z-1 absolute top-0 bottom-2 left-0 right-0 origin-top-left"></div>
-        <h2 className="text-2xl font-black text-shadow-lg">
-          Fix Your Zoom Rooms
-        </h2>
-        <div className="text-shadow-lg">
-          <p className="inline">With </p>
-          <h1 className="inline">{config.appName}</h1>
-          <p className="inline">
-            , teachers can receive questions, send polls, and keep large groups
-            of students engaged from one realtime dashboard.
-          </p>
-        </div>
-      </div>
-      <p>Teacher?</p>
-      <button onClick={teacherSignIn}>Start a Room</button>
-      <p>Student?</p>
-      <button>Join {config.appName} Room</button>
+      <nav className="mb-6">
+        <LogoType color="white" className="ml-3 mt-3.5 inline-block" />
+      </nav>
+      <h2 className={classNames(styles.heroText, 'mb-5')}>
+        Level Up Your
+        <br />
+        Zoom Rooms
+      </h2>
+      <p className="text-base leading-1 text-center tracking-tighter mb-12">
+        Dashbrain helps everyone stay in-sync over video calls.
+      </p>
+      <FeatureItem
+        imgSrc="assets/hand-icon.png"
+        imgAlt="Raise Hand"
+        imgClassName="w-4"
+        className="w-56 mx-auto mb-4"
+        description="Notifications when someone raises their hand."
+      />
+      <FeatureItem
+        imgSrc="assets/poll-icon.png"
+        imgAlt="Poll"
+        imgClassName="w-4.5"
+        className="w-56.5 mx-auto mb-4"
+        description="Send polls — make sure everyone’s on the same page."
+      />
+      <FeatureItem
+        imgSrc="assets/message-icon.png"
+        imgAlt="Send Message"
+        imgClassName="w-4.5"
+        className="w-56.5 mx-auto mb-6.5"
+        description="Messages to specific groups, even in breakout rooms."
+      />
+      <p className="text-center mb-10">. . . and more!</p>
+      <p className="font-black tracking-tighter text-center mb-1.5">Teacher?</p>
+      <p className="w-76 leading-none tracking-tighter text-center mx-auto mb-4">
+        Dashbrain works with any video-calling software. Just open a new
+        Dashbrain, send the code to your students, and get connected!
+      </p>
+      <IndexButton
+        onClick={teacherSignIn}
+        title="Create a Dashboard"
+        className="block mx-auto mb-11"
+      />
+      <p className="font-black tracking-tighter text-center mb-1.5">Student?</p>
+      <IndexButton
+        onClick={teacherSignIn}
+        title="Join a Dashboard"
+        className="block mx-auto mb-12"
+      />
+      <div className={styles.backgroundTiles}></div>
+      <div className={styles.backgroundGradient}></div>
     </div>
   );
 
