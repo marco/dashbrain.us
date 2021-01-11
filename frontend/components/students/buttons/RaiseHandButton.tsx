250dda171818331b09547e90c4ae63e0a1ffd09c
Marco Burstein <marco@marco.how>
2021-01-11 13:17:02 -0800

    Create new `RaiseHandButton` student styles.

--- frontend/components/students/buttons/RaiseHandButton.tsx
+++ frontend/components/students/buttons/RaiseHandButton.tsx
@@ -3,6 +3,9 @@ import firebase from 'firebase/app';
 import * as rooms from '../../../lib/rooms';
 import * as events from '../../../lib/events';
 import * as eventSender from '../../../lib/event-sender';
+import sharedStyles from '../../../../styles/pages/teachers-students.module.scss';
+import styles from './RaiseHandButton.module.scss';
+import classNames from 'classnames';
 
 let RaiseHandButton: React.FC<{ room: rooms.Room; events: events.Event[] }> = (
   props
@@ -13,11 +16,26 @@ let RaiseHandButton: React.FC<{ room: rooms.Room; events: events.Event[] }> = (
       event.senderUid == firebase.auth().currentUser?.uid
   ) as events.EventHand | undefined;
 
-  if (raiseEvent) {
-    return <button onClick={onClick}>Lower Hand</button>;
-  } else {
-    return <button onClick={onClick}>Raise Hand</button>;
-  }
+  return (
+    <button
+      onClick={onClick}
+      className={classNames(
+        styles.button,
+        sharedStyles.bottomButtonSquare,
+        'ml-1.5 flex-1 flex flex-col',
+        { [sharedStyles.bottomButtonSquareInsetShadow]: raiseEvent }
+      )}
+    >
+      <div className="flex-1 items-center justify-center flex">
+        <img
+          src={'/assets/hand/blue.png'}
+          alt="Raise Hand"
+          className="h-14 ml-1 mt-1"
+        />
+      </div>
+      {raiseEvent ? 'Lower Hand' : 'Raise Hand'}
+    </button>
+  );
 
   async function onClick() {
     if (raiseEvent) {
