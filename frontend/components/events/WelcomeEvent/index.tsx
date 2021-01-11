a623eb77c1d1691500644e277d9cac3d53f21fc2
Marco Burstein <marco@marco.how>
2021-01-11 11:27:01 -0800

    Update welcome copy and styles.

--- frontend/components/events/WelcomeEvent/index.tsx
+++ frontend/components/events/WelcomeEvent/index.tsx
@@ -4,6 +4,7 @@ import { Room } from '../../../lib/rooms';
 import eventStyles from '../events.module.scss';
 import styles from './event.module.scss';
 import classNames from 'classnames';
+import firebase from 'firebase/app';
 
 let WelcomeEvent: React.FC<{ className?: string; room: Room }> = (props) => {
   return (
@@ -14,17 +15,39 @@ let WelcomeEvent: React.FC<{ className?: string; room: Room }> = (props) => {
         className={eventStyles.iconLarge}
       />
       <p className="font-bold">Welcome to Dashbrain!</p>
-      <p>Your Dashbrain code is {props.room.id}.</p>
-      <p className="mt-1">
-        You will receive notifications in this list as students join, ask
-        questions, raise their hands, and more!
-      </p>
-      <p className="mt-1">
-        Below, you can start a poll, send private messages to students, or
-        export this Dashbrain as a PDF.
-      </p>
+      {getCopy()}
     </div>
   );
+
+  function getCopy() {
+    if (props.room.teacherUid === firebase.auth().currentUser?.uid) {
+      return (
+        <>
+          <p>Your Dashbrain code is {props.room.id}.</p>
+          <p className="mt-3">
+            You will receive notifications in this list as students join, ask
+            questions, raise their hands, and more!
+          </p>
+          <p className="mt-3">
+            Below, you can start a poll, send private messages to students, or
+            export this Dashbrain as a PDF.
+          </p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <p>
+            You will receive notifications in this list as classmates join, ask
+            questions, raise their hands, and more!
+          </p>
+          <p className="mt-3">
+            Below, you can ask a question, raise your hand, or send a message.
+          </p>
+        </>
+      );
+    }
+  }
 };
 
 export default WelcomeEvent;
