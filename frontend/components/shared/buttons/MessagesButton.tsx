efc59944001da09a740c581a0015401f79f4c311
Marco Burstein <marco@marco.how>
2021-01-10 21:40:42 -0800

    Create new `MessagesButton`.

--- frontend/components/shared/buttons/MessagesButton.tsx
+++ frontend/components/shared/buttons/MessagesButton.tsx
@@ -2,9 +2,27 @@ import React, { useState } from 'react';
 import { Event } from '../../../lib/events';
 import { Room } from '../../../lib/rooms';
 import MessagesSheet from '../sheets/MessagesSheet';
+import classNames from 'classnames';
+import styles from './MessagesButton.module.scss';
+import sharedStyles from '../../../../styles/pages/teachers-students.module.scss';
 
-let MessagesButton: React.FC<{ onClick: () => void }> = (props) => {
-  return <button onClick={props.onClick}>Messages</button>;
+let MessagesButton: React.FC<{ onClick: () => void; flatStyle: boolean }> = (
+  props
+) => {
+  return (
+    <div
+      onClick={props.onClick}
+      className={classNames({
+        [sharedStyles.bottomButtonFlat]: props.flatStyle,
+        [styles.flatButton]: props.flatStyle,
+      })}
+    >
+      {props.flatStyle ? (
+        <img src={'/assets/message/green.png'} alt="Messages" className="w-4" />
+      ) : null}
+      Messages
+    </div>
+  );
 };
 
 export default MessagesButton;
