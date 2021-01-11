20b7f92b8f9367033badf1bdbd086c8dcce87414
Marco Burstein <marco@marco.how>
2021-01-10 17:21:17 -0800

    Create `StudentJoinEvent` component.

---
+++ frontend/components/events/StudentJoinEvent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { EventStudentJoin } from '../../lib/events';
+import { Room } from '../../lib/rooms';
+
+let StudentJoinEvent: React.FC<{
+  room: Room;
+  event: EventStudentJoin;
+}> = (props) => {
+  return (
+    <div>
+      {props.room.students[props.event.studentUid]?.name || 'A student'} joined
+      the Dashbrain.
+    </div>
+  );
+};
+
+export default StudentJoinEvent;
