e1088e728aab86e23409cefe61eaea48e594de07
Marco Burstein <marco@marco.how>
2021-01-10 16:31:36 -0800

    Create index page `FeatureItem` component.

---
+++ frontend/components/index/FeatureItem.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React from 'react';
+
+let FeatureItem: React.FC<{
+  imgSrc: string;
+  imgAlt: string;
+  imgClassName: string;
+  className?: string;
+  description: string;
+}> = (props) => {
+  return (
+    <div
+      className={classNames(
+        'flex justify-between items-start text-left',
+        props.className
+      )}
+    >
+      <img
+        src={props.imgSrc}
+        alt={props.imgAlt}
+        className={props.imgClassName}
+      />
+      <p className="w-48 tracking-tighter leading-none">{props.description}</p>
+    </div>
+  );
+};
+
+export default FeatureItem;
