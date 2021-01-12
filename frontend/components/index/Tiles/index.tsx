bba965b10b69ac2e659ac74f9967d7a0a4a01aca
Marco Burstein <marco@marco.how>
2021-01-11 19:54:14 -0800

    Create `IndexTiles` component.

---
+++ frontend/components/index/Tiles/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+let IndexTiles: React.FC = () => {
+  return <div className={styles.backgroundTiles}></div>;
+};
+
+export default IndexTiles;
