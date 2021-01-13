c606e278f651c117b6a809bb4a9ecd4293a698d0
Marco Burstein <marco@marco.how>
2021-01-13 07:52:48 -0800

    Create `analytics.ts`.

---
+++ frontend/lib/analytics.ts
@@ -0,0 +1,35 @@
+import firebase from 'firebase/app';
+import { Event } from './events';
+declare let gtag: any;
+
+export function sendEventNewRoom(id: string) {
+  gtag('event', 'custom_new_room', {
+    event_category: 'teachers',
+    event_label: `{ "uid": "${
+      firebase.auth().currentUser?.uid || ''
+    }", "roomId": "${id}" }`,
+  });
+}
+
+export function sendEventJoinRoom(id: string) {
+  gtag('event', 'custom_join_room', {
+    event_category: 'students',
+    event_label: `{ "uid": "${
+      firebase.auth().currentUser?.uid || ''
+    }", "roomId": "${id}" }`,
+  });
+}
+
+export function sendEventForEvent(event: Event) {
+  gtag('event', 'custom_send_event_' + event.type, {
+    event_category: 'events',
+    event_label: `{ "senderUid": "${event.senderUid}", "id": "${event.id}" }`,
+  });
+}
+
+export function sendEventForDeleteEvent(event: Event) {
+  gtag('event', 'custom_delete_event_' + event.type, {
+    event_category: 'events',
+    event_label: `{ "senderUid": "${event.senderUid}", "id": "${event.id}" }`,
+  });
+}
