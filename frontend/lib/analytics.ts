e224d2006b9a7594de510e009a1897ad2446af38
Marco Burstein <marco@marco.how>
2021-01-16 20:41:50 -0800

    Update analytics to use suffix according to vocab.

--- frontend/lib/analytics.ts
+++ frontend/lib/analytics.ts
@@ -1,16 +1,17 @@
 import firebase from 'firebase/app';
 import { Event } from './events';
+import * as vocab from './vocabulary';
 declare let gtag: any;
 
 export function sendEventFirstUserSignIn(id: string) {
-  gtag('event', 'custom_user_first_sign_in', {
+  gtag('event', 'custom_user_first_sign_in' + suffix(), {
     event_category: 'teachers',
     event_label: `{ "uid": "${firebase.auth().currentUser?.uid || ''}" }`,
   });
 }
 
 export function sendEventNewRoom(id: string) {
-  gtag('event', 'custom_new_room', {
+  gtag('event', 'custom_new_room' + suffix(), {
     event_category: 'teachers',
     event_label: `{ "uid": "${
       firebase.auth().currentUser?.uid || ''
@@ -19,7 +20,7 @@ export function sendEventNewRoom(id: string) {
 }
 
 export function sendEventJoinRoom(id: string) {
-  gtag('event', 'custom_join_room', {
+  gtag('event', 'custom_join_room' + suffix(), {
     event_category: 'students',
     event_label: `{ "uid": "${
       firebase.auth().currentUser?.uid || ''
@@ -28,15 +29,23 @@ export function sendEventJoinRoom(id: string) {
 }
 
 export function sendEventForEvent(event: Event) {
-  gtag('event', 'custom_send_event_' + event.type, {
+  gtag('event', 'custom_send_event_' + event.type + suffix(), {
     event_category: 'events',
     event_label: `{ "senderUid": "${event.senderUid}", "id": "${event.id}" }`,
   });
 }
 
 export function sendEventForDeleteEvent(event: Event) {
-  gtag('event', 'custom_delete_event_' + event.type, {
+  gtag('event', 'custom_delete_event_' + event.type + suffix(), {
     event_category: 'events',
     event_label: `{ "senderUid": "${event.senderUid}", "id": "${event.id}" }`,
   });
+}
+
+function suffix(): string {
+  if (vocab.isSchool()) {
+    return '_school';
+  } else {
+    return '';
+  }
 }
